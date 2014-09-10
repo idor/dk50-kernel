c73d104ddba7836aeb5f122c64b9466dd849424b
Harshesh Valera <hvalera@intrinsyc.com>
2014-09-09 20:59:01 -0700

    Read MAC Address from kernel commandline
    Change-Id: I8ec66fabc1b584c4c5515dd13b57c602a62a3cf3
    Signed-off-by: Harshesh Valera <hvalera@intrinsyc.com>

--- arch/arm/mach-msm/board-8974-gpiomux.c
+++ arch/arm/mach-msm/board-8974-gpiomux.c
@@ -13,6 +13,7 @@
 
 #include <linux/init.h>
 #include <linux/ioport.h>
+#include <linux/export.h>
 #include <mach/board.h>
 #include <mach/gpio.h>
 #include <mach/gpiomux.h>
@@ -1467,3 +1468,41 @@ void __init msm_8974_init_gpiomux(void)
 		msm_gpiomux_install(apq8074_dragonboard_ts_config,
 			ARRAY_SIZE(apq8074_dragonboard_ts_config));
 }
+
+unsigned char wlan_mac[6];
+int valid_wlan_mac = 0;
+static int __init wlan_init_mac(char *str)
+{
+       int i, j;
+       short byte1, byte0;
+       pr_info("WLAN init with mac address: %s\n", str);
+       if (str == NULL) {
+               return -1;
+       }
+
+       for (i=0, j=0; i < 6; i++,j=j+2) {
+               if ((byte1 = hex_to_bin(*(str+j))) < 0)
+                       return -1;
+
+               if ((byte0 = hex_to_bin(*(str+j+1))) < 0)
+                       return -1;
+
+               wlan_mac[i] = (unsigned char)(byte1*16+byte0);
+       }
+       pr_info("WLAN convert to %02x:%02x:%02x:%02x:%02x:%02x\n",
+               wlan_mac[0], wlan_mac[1], wlan_mac[2], wlan_mac[3], wlan_mac[4], wlan_mac[5]);
+       valid_wlan_mac = 1;
+       return 0;
+}
+
+unsigned char* qcom_get_wlan_mac(unsigned char *buf)
+{
+       if (!valid_wlan_mac)
+               return NULL;
+
+       memcpy(buf, wlan_mac, 6);
+       return buf;
+
+}
+early_param("wlan_mac", wlan_init_mac);
+EXPORT_SYMBOL(qcom_get_wlan_mac);
